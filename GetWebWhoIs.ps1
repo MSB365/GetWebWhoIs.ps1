3b5ce40ee7c42c4f2ed822bebb19c26d1d2fcf1a
Drago Petrovic <petrovic@chur.gr>
2017-04-27 09:12:36 +0200

    Add files via upload

---
+++ GetWebWhoIs.ps1
@@ -0,0 +1,70 @@
+﻿ <#
+.SYNOPSIS
+GetWebWhoIs.ps1 
+
+.DESCRIPTION 
+Performs a domain name lookup and returns information such as
+domain availability (creation and expiration date),
+domain ownership, name servers, etc..
+
+
+.PARAMETER
+Specifies the domain name (enter the domain name without http:// and www (e.g. abstergo.ch))
+
+
+.EXAMPLE
+.\GetWebWhoIs.ps1 abstergo.ch
+
+
+.NOTES
+Written by: Nikolay Petkov
+Edited by: Drago Petrovic
+
+.TROUBLENOTES
+
+
+
+Find me on:
+
+* LinkedIn:	https://www.linkedin.com/in/drago-petrovic-86075730/
+* Xing:     https://www.xing.com/profile/Drago_Petrovic
+* Website:  https://blog.abstergo.ch
+* GitHub:   https://github.com/MSB365
+
+
+Change Log
+V1.00, 20/12/2014 - Initial Version
+V2.00, 27/04/2017 - Modified to run without multistepps
+
+
+--- keep it simple, but significant ---
+
+.COPYRIGHT
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#>
+
+
+param (
+                [Parameter(Mandatory=$True,
+                           HelpMessage='Please enter domain name (e.g. microsoft.com)')]
+                           [string]$domain
+        )
+Write-Host "Connecting to Web Services URL..." -ForegroundColor Green
+try {
+#Retrieve the data from web service WSDL
+If ($whois = New-WebServiceProxy -uri "http://www.webservicex.net/whois.asmx?WSDL") {Write-Host "Ok" -ForegroundColor Green}
+else {Write-Host "Error" -ForegroundColor Red}
+Write-Host "Gathering $domain data..." -ForegroundColor Green
+#Return the data
+(($whois.getwhois("$domain")).Split("<<<")[0])
+} catch {
+Write-Host "Please enter valid domain name (e.g. microsoft.com)." -ForegroundColor Red}
+ #end function WhoIs
